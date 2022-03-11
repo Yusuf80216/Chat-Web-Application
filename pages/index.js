e9769e21fdf88a6d84078bb98fed901d4d93d4ab
Yusuf Ansari <67997835+Yusuf80216@users.noreply.github.com>
2022-03-11 11:21:20 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Auth() {
     <div className="background">
       <div className="auth-container">
         <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
-          <div className="auth-title">NextJS Chat</div>
+          <div className="auth-title">Techs Chat</div>
 
           <div className="input-container">
             <input
